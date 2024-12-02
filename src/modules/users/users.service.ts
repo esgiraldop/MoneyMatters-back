38a7038e6d00a54d49c44e11d530f3cabd17c58a
Erick Giraldo <erickgiraldo94@gmail.com>
2024-12-02 16:51:57 -0500

    feature: find user by id service created

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./entities/user.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>
+  ) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return "This action adds a new user";
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findById(userId: number) {
+    const user = this.userRepository.findOne({ where: { id: userId } });
+
+    if (!user)
+      throw new NotFoundException(`The user with id ${userId} does not exists`);
+
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
