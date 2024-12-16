421cf1c63ef88325cab2ee518751389390e3147e
Erick Giraldo <erickgiraldo94@gmail.com>
2024-12-16 16:44:12 -0500

    feat: small change to dto

--- src/modules/transactions/dto/update-transaction.dto.ts
+++ src/modules/transactions/dto/update-transaction.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from "@nestjs/swagger";
 import { CreateTransactionDto } from "./create-transaction.dto";
-import { IsInt, IsNotEmpty, IsPositive } from "class-validator";
 
 export class UpdateTransactionDto extends PartialType(CreateTransactionDto) {}
